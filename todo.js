f391c58c035898654a2d7489b2071cec7ddc6f34
Zain Syed <zeddotes@gmail.com>
2015-10-07 01:20:02 -0400

    - added firebase

--- todo.js
+++ todo.js
@@ -1,19 +1,41 @@
 
-    var app = angular.module('ToDoApp',[]);
+    var app = angular.module('ToDoApp',['firebase']);
 
 
-app.controller('TaskController', ['$scope', function($scope) {
-  $scope.title = 'MyToDoAppInitial!';
-    
-    $scope.tasks = [];
-    
-    $scope.insert= function(task){
+app.controller('TaskController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+    $scope.title = 'MyToDoAppInitial!';
+        
+    // add reference to firebase collection -- mine is zeddotes/test
+    var ref = new Firebase("https://zeddotes.firebaseio.com/test");
+
+    // if $firebaseArray(ref) returns empty, just use a new array instead
+    $scope.tasks = $firebaseArray(ref) || [];
+
+    // clear the default
+    $scope.newTask = {};
+
+    // main adding method which will check if $scope.newTask exists and add to firebase.
+    // it should also clean out the $scope.newTask form
+    $scope.addTask = function(){
+      if ($scope.newTask){
+        // firebase method to add data
+        $scope.tasks.$add({
+          addedOn: $.now(),
+          content: $scope.newTask.content
+        });
+
+        $scope.newTask = {};
+      }
+    };
+
+    // $scope.insert= function(task){
      
-        var tl = angular.copy(task);
-        tl.addedOn = new Date();
-        $scope.tasks.push(tl);
-        task.content = "";
-        }
+    //     var tl = angular.copy(task);
+    //     tl.addedOn = new Date();
+    //     // $scope.tasks.push(tl);
+    //     $scope.tasks.$add(tl);
+    //     task.content = "";
+    //     }
     
     
     $scope.destroy = function(task){
